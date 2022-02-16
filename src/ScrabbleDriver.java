6a1bbc2a1bddea026633c2962384ce291ce3fc3b
AsharDilan <ashardi@ssfs.org>
2022-02-16 10:54:38 -0500

    Could you take a look at the Scrabble Helper and Driver code? I am trying to write the code to the best of my ability, but I am not confident that what I am doing is correct. If you could let me know if what I am doing is correct and how to resolve the issues with my code, that would be great.

--- src/ScrabbleDriver.java
+++ src/ScrabbleDriver.java
@@ -1,7 +1,23 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ScrabbleDriver {
-
-
+    public static void main(String[] args) {
+        ScrabbleHelper WordList = new ScrabbleHelper();
+        System.out.println(ScrabbleHelper.getWordList().get(66349));
+    }
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a word to test");
+    String word = scan.nextLine();
+    Boolean word = isValidWord(word);
+        if () {
+        String word = WordList(word);
+        System.out.println(word + "is a word");
+    } else {
+        System.out.println(word + "is not a word");
+    }
+        System.out.println("Goob-bye");
 }
--- src/ScrabbleHelper.java
+++ src/ScrabbleHelper.java
@@ -1,12 +1,33 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOError;
+import javax.annotation.processing.Filer;
+import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ScrabbleHelper {
+    private ArrayList wordList;
+    public static void ScrabbleHelper() throws IOException{
+        Scanner scan = new Scanner(new File("files/enable.txt"));
+        FileWriter wordList = new FileWriter("enable.txt");
+    }
+    public ArrayList getWordList(){
+        return wordList;
+    }
+    public static String foundWord(String[] elements, String word);{
+        while (){
+            if (word = wordList)
+                return true;
+            else
+                return false;
+        }
+    }
+    public static String findMatches(String pattern){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word pattern:");
+        String pattern = scan.nextLine();
+        for (int i = 0; i < pattern.length(), int x)
 
+    }
 
 }
 
